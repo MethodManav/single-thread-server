6e367a3195cd2d0155f7f15ec60016f4edb50e2b
Behera Manav <manavbehera123@gmail.com>
2025-05-17 15:09:38 +0530

    created server

---
+++ Server.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Server {
+
+    @SuppressWarnings("CallToPrintStackTrace")
+    public void run() {
+        int port = 8010;
+        try {
+            ServerSocket socket = new ServerSocket(port);
+            socket.setSoTimeout(10000);
+            while (true) {
+                System.out.println("Server is listening to port " + port);
+                Socket acceptedConnection = socket.accept();
+                System.out.println("Connection Accepted from client " + acceptedConnection.getRemoteSocketAddress());
+                PrintWriter toClient = new PrintWriter(acceptedConnection.getOutputStream());
+                BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()));
+                toClient.println("Hello From Server");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
